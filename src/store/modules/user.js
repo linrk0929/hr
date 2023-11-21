bbe84fd90a5d84968ec2e1c998bce1bc005f7368
lrk <1196824608@qq.com>
2023-11-21 16:57:49 +0800

    主页健全

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,5 +1,5 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
-
+import { login } from '@/api/user'
 const state = {
   token: getToken() // 从缓存中读取初始值
 }
@@ -16,11 +16,12 @@ const mutations = {
   }
 }
 const actions = {
-  login(context, data) {
+  async login(context, data) {
     // context上下文，传入参数
     console.log(data)
+    const token = await login(data)
     // todo:调用登录接口
-    context.commit('setToken', '123456')
+    context.commit('setToken', token)
   }
 
 }
